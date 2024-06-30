a63d0c36ff99bef63f9d3fc21ed16925617bd50e
Alexander Koller <akoller@gmail.com>
2024-06-30 09:02:54 +0200

    fixed trust_remote_code issue (I hope)

--- graph-dependency-parser/min_train.py
+++ graph-dependency-parser/min_train.py
@@ -1,16 +1,18 @@
 import sys
-xa=zip
-xV=set
-xp=enumerate
-xY=None
-xi=max
-xd=len
-xC=str
-xP=int
-xn=range
-xq=True
-xR=list
-xk=float
+oA=range
+ob=len
+oI=zip
+oV=print
+os=set
+oX=enumerate
+oT=None
+ox=max
+oF=str
+oB=int
+oY=True
+oW=list
+oS=StopIteration
+of=float
 from collections import defaultdict
 import pydantic
 import torch
@@ -22,185 +24,199 @@ import wandb
 from torch import LongTensor
 from torch.nn import Linear,CrossEntropyLoss,Module
 from torch.optim import Adam
-from transformers import AutoTokenizer,RobertaForTokenClassification,RobertaTokenizer,RobertaTokenizerFast,RobertaModel,XLMRobertaTokenizerFast
+from transformers import AutoTokenizer,RobertaForTokenClassification,RobertaTokenizer,RobertaTokenizerFast, RobertaModel,XLMRobertaTokenizerFast
 from ufal.chu_liu_edmonds import chu_liu_edmonds
 from datasets import load_dataset
 from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
 from util import Config
-def strip_none_heads(examples,i):
- x=examples["tokens"][i]
- H=examples["head"][i]
- B=examples["deprel"][i]
- D=[(t,h,d)for t,h,d in xa(x,H,B)if h!="None"]
- return xa(*D)
-def map_first_occurrence(nums):
- c=xV()
- return{num:i for i,num in xp(nums)if num is not xY and num not in c and not c.add(num)}
-def pad_to_same_size(lists,padding_symbol):
- J=xi([xd(l)for l in lists])
- return[l+(padding_symbol)*(J-xd(l))for l in lists]
-def train(config:Config,UD_DATASET:xC,u:Module):
- def accuracy(xw,xg,ignore_index=config.ignore_index)->(xP,xP):
-  w=torch.sum(xg!=ignore_index)
-  g=torch.sum(xg==xw)
-  return xP(g),xP(w)
- def tokenize_and_align_labels(examples,skip_index=config.ignore_index):
-  X,examples_heads,examples_deprels=[],[],[]
-  for h in xn(xd(examples["tokens"])):
-   tt,hh,dd=strip_none_heads(examples,h)
-   X.append(tt)
-   examples_heads.append(hh)
-   examples_deprels.append(dd)
-  F=j(X,truncation=xq,is_split_into_words=xq,padding=xq)
-  v=[]
-  l=[]
-  t=[]
-  xX:xR[xP]=[]
-  b=0
-  for h,annotated_heads in xp(examples_heads):
-   B=examples_deprels[h]
-   s=F.word_ids(batch_index=h)
-   a=map_first_occurrence(s)
-   V=xY
-   Y:xR[xP]=[]
-   deprel_ids_here:xR[xP]=[]
-   tokens_representing_word_here:xR[xP]=[0]
-   for p,word_idx in xp(s):
-    if word_idx is xY:
-     Y.append(skip_index)
-     deprel_ids_here.append(skip_index)
-    elif word_idx!=V:
-     if annotated_heads[word_idx]=="None":
-      print("A 'None' head survived!")
+def o(instances):
+ def U(deprel_id):
+  return "--" if deprel_id==-100 else X[deprel_id]
+ for i in oA(ob(instances["input_ids"])):
+  i=F.convert_ids_to_tokens(instances["input_ids"][i])
+  M=instances["head"][i]
+  n=[U(d)for d in instances["deprel_ids"][i]]
+  N=instances["tokenid_to_wordid"][i]
+  for i,t,h,d in oI(oA(ob(i)),i,M,n):
+   G=N[i]if i<ob(N)else "--"
+   oV(f"{i}\t{t}\t{h}\t{d}\t{wordpos}")
+  oV()
+def u(examples,i):
+ ii=examples["tokens"][i]
+ M=examples["head"][i]
+ n=examples["deprel"][i]
+ P=[(t,h,d)for t,h,d in oI(ii,M,n)if h!="None"]
+ return oI(*P)
+def w(nums):
+ t=os()
+ return{num:i for i,num in oX(nums)if num is not oT and num not in t and not t.add(num)}
+def e(lists,padding_symbol):
+ R=ox([ob(l)for l in lists])
+ return[l+(padding_symbol,)*(R-ob(l))for l in lists]
+def train(config:Config,UD_DATASET:oF,Y:Module):
+ def q(od,om,ignore_index=config.ignore_index)->(oB,oB):
+  g=torch.sum(om!=ignore_index) 
+  J=torch.sum(om==od) 
+  return oB(J),oB(g)
+ def z(examples,skip_index=config.ignore_index):
+  h,a,l=[],[],[]
+  for y in oA(ob(examples["tokens"])):
+   tt,hh,dd=u(examples,y)
+   h.append(tt)
+   a.append(hh)
+   l.append(dd)
+  H=F(h,truncation=oY,is_split_into_words=oY,padding=oY) 
+  C=[] 
+  d=[]
+  m=[]
+  r:oW[oB]=[]
+  p=0 
+  for y,annotated_heads in oX(a):
+   n=l[y]
+   O=H.word_ids(batch_index=y)
+   j=w(O) 
+   E=oT
+   K:oW[oB]=[]
+   Q:oW[oB]=[]
+   c:oW[oB]=[0]
+   for D,word_idx in oX(O):
+    if word_idx is oT:
+     K.append(skip_index)
+     Q.append(skip_index)
+    elif word_idx!=E:
+     if annotated_heads[word_idx]=="None": 
+      oV("A 'None' head survived!")
       sys.exit(0)
      else:
-      i=xP(annotated_heads[word_idx])
-      d=0 if i==0 else a[i-1]
-      Y.append(d)
-      deprel_ids_here.append(R[B[word_idx]])
-      tokens_representing_word_here.append(p)
+      A=oB(annotated_heads[word_idx])
+      b=0 if A==0 else j[A-1]
+      K.append(b)
+      Q.append(T[n[word_idx]])
+      c.append(D) 
     else:
-     Y.append(skip_index)
-     deprel_ids_here.append(skip_index)
-    V=word_idx
-   v.append(Y)
-   l.append(deprel_ids_here)
-   t.append(tokens_representing_word_here)
-   xX.append(xd(tokens_representing_word_here))
-   if xd(tokens_representing_word_here)>b:
-    b=xd(tokens_representing_word_here)
-  for C in t:
-   C+=[-1]*(b-xd(C))
-  F["head"]=v
-  F["deprel_ids"]=l
-  F["tokens_representing_words"]=t
-  F["num_words"]=xX
-  F["tokenid_to_wordid"]=[F.word_ids(batch_index=i)for i in xn(xd(examples_heads))]
-  return F
+     K.append(skip_index)
+     Q.append(skip_index)
+    E=word_idx
+   C.append(K)
+   d.append(Q)
+   m.append(c)
+   r.append(ob(c))
+   if ob(c)>p:
+    p=ob(c)
+  for I in m:
+   I+=[-1]*(p-ob(I))
+  H["head"]=C
+  H["deprel_ids"]=d
+  H["tokens_representing_words"]=m
+  H["num_words"]=r
+  H["tokenid_to_wordid"]=[H.word_ids(batch_index=i)for i in oA(ob(a))] 
+  return H 
  import os
  os.environ["TOKENIZERS_PARALLELISM"]="false"
  if torch.cuda.is_available():
-  P="cuda:"+os.getenv("CUDA",default=config.cuda_device)
-  n=torch.device(P)
-  print("Running on CUDA device "+P)
+  V="cuda:"+os.getenv("CUDA",default="0")
+  s=torch.device(V)
+  oV(f"Running on CUDA device {s}")
  elif torch.backends.mps.is_available()and torch.backends.mps.is_built():
-  n="mps"
-  print("Running on MPS.")
+  s="mps"
+  oV("Running on MPS.")
  else:
-  n="cpu"
-  print("Running on CPU.")
-  print("If you're on a Mac, check that you have MacOS 12.3+, an MPS-enabled chip, and current Pytorch.")
- q=["acl","acl:relcl","advcl","advcl:relcl","advmod","advmod:emph","advmod:lmod","amod","appos","aux","aux:pass","case","cc","cc:preconj","ccomp","clf","compound","compound:lvc","compound:prt","compound:redup","compound:svc","conj","cop","csubj","csubj:outer","csubj:pass","dep","det","det:numgov","det:nummod","det:poss","discourse","dislocated","expl","expl:impers","expl:pass","expl:pv","fixed","flat","flat:foreign","flat:name","goeswith","iobj","list","mark","nmod","nmod:poss","nmod:tmod","nsubj","nsubj:outer","nsubj:pass","nummod","nummod:gov","obj","obl","obl:agent","obl:arg","obl:lmod","obl:tmod","orphan","parataxis","punct","reparandum","root","vocative","xcomp","det:predet","obl:npmod","nmod:npmod"]
- R={rel:i for i,rel in xp(q)}
- k=load_dataset("universal_dependencies",UD_DATASET)
- j=XLMRobertaTokenizerFast.from_pretrained("xlm-roberta-base",add_prefix_space=xq)
- y=k.map(tokenize_and_align_labels,batched=xq,batch_size=config.batchsize)
- def print_instances(instances):
-  def deprel_or_skip(deprel_id):
-   return "--" if deprel_id==-100 else q[deprel_id]
-  for i in xn(xd(instances["input_ids"])):
-   x=j.convert_ids_to_tokens(instances["input_ids"][i])
-   H=instances["head"][i]
-   B=[deprel_or_skip(d)for d in instances["deprel_ids"][i]]
-   f=instances["tokenid_to_wordid"][i]
-   for i,t,h,d in xa(xn(xd(x)),x,H,B):
-    Q=f[i]if i<xd(f)else "--"
- u=u.to(n)
- E=['input_ids','attention_mask','head','deprel_ids','tokens_representing_words','num_words','tokenid_to_wordid']
- O=y["train"]
- O.set_format(type='torch',columns=E)
- G=torch.utils.data.DataLoader(O,batch_size=config.batchsize)
- M=y["validation"]
- M.set_format(type='torch',columns=E)
- S=torch.utils.data.DataLoader(M,batch_size=config.batchsize)
- T=CrossEntropyLoss(ignore_index=config.ignore_index,reduction="mean")
- U=(config.betas[0],config.betas[1])
- z=Adam(u.parameters(),lr=config.learning_rate,betas=U)
- r=config.model_dump()|{"betas":U,"dataset":UD_DATASET}
- wandb.init(project=config.wandb_project,config=r)
- wandb.watch(u,log='gradients',log_freq=5)
- for I in xn(config.epochs):
-  N=0.0
-  u.train()
-  for o in tqdm.tqdm(G,desc="Training, Epoch "+xC(I)):
-   A=o["input_ids"].to(n)
-   W=o["attention_mask"].to(n)
-   m=u(A,W)
-   H=o["head"].to(n)
-   K=T(m,H)
-   wandb.log({"batch_loss":K})
-   z.zero_grad()
-   K.backward()
-   z.step()
-   if config.debug:
+  s="cpu"
+  oV("Running on CPU.")
+  oV("If you're on a Mac, check that you have MacOS 12.3+, an MPS-enabled chip, and current Pytorch.")
+ X=["acl","acl:relcl","advcl","advcl:relcl","advmod","advmod:emph","advmod:lmod","amod","appos","aux","aux:pass","case","cc","cc:preconj","ccomp","clf","compound","compound:lvc","compound:prt","compound:redup","compound:svc","conj","cop","csubj","csubj:outer","csubj:pass","dep","det","det:numgov","det:nummod","det:poss","discourse","dislocated","expl","expl:impers","expl:pass","expl:pv","fixed","flat","flat:foreign","flat:name","goeswith","iobj","list","mark","nmod","nmod:poss","nmod:tmod","nsubj","nsubj:outer","nsubj:pass","nummod","nummod:gov","obj","obl","obl:agent","obl:arg","obl:lmod","obl:tmod","orphan","parataxis","punct","reparandum","root","vocative","xcomp","det:predet","obl:npmod","nmod:npmod"]
+ T={rel:i for i,rel in oX(X)}
+ x=load_dataset("universal_dependencies",UD_DATASET,trust_remote_code=oY)
+ F=XLMRobertaTokenizerFast.from_pretrained("xlm-roberta-base",add_prefix_space=oY)
+ B=x.map(z,batched=oY,batch_size=config.batchsize) 
+ Y=Y.to(s)
+ W=['input_ids','attention_mask','head','deprel_ids','tokens_representing_words','num_words','tokenid_to_wordid']
+ S=B["train"]
+ S.set_format(type='torch',columns=W)
+ f=torch.utils.data.DataLoader(S,batch_size=config.batchsize)
+ k=B["validation"]
+ k.set_format(type='torch',columns=W)
+ oU=torch.utils.data.DataLoader(k,batch_size=config.batchsize)
+ ou=CrossEntropyLoss(ignore_index=config.ignore_index,reduction="mean")
+ ow=(config.betas[0],config.betas[1])
+ oe=Adam(Y.parameters(),lr=config.learning_rate,betas=ow)
+ ov=config.model_dump()|{"betas":ow,"optimizer":"Adam","architecture":"xlm-roberta+linear","dataset":UD_DATASET}
+ wandb.init(project="roberta-parsing",config=ov)
+ wandb.watch(Y,log='gradients',log_freq=5)
+ for oL in oA(config.epochs):
+  oi=0.0
+  oM=0
+  on=0
+  Y.train()
+  for oN in tqdm.tqdm(f,desc=f"Training, Epoch {oL}"):
+   oP=oN["input_ids"].to(s)
+   ot=oN["attention_mask"].to(s)
+   oR=oP.shape[0]
+   oM+=oR
+   og=Y(oP,ot)
+   M=oN["head"].to(s)
+   oJ=ou(og,M)
+   wandb.log({"batch_loss":oJ})
+   oe.zero_grad()
+   oJ.backward()
+   oe.step()
+   if oM>=config.limit_train:
     break
-  u.eval()
+   if config.debug:
+    break 
+  Y.eval()
   with torch.no_grad():
-   L=0
-   xH=0
-   xB=0
-   xD=0
-   xc=0
-   xJ=0
-   for o in tqdm.tqdm(S,desc="Evaluating"):
-    A=o["input_ids"].to(n)
-    W=o["attention_mask"].to(n)
-    m=u(A,W)
-    xw=torch.argmax(m,dim=1).view(-1)
-    xg=o["head"].to(n).view(-1)
-    g,w=accuracy(xw,xg)
-    L+=xP(g)
-    xH+=xP(w)
-    xB+=xP(xw.shape[0])
-    for i in xn(m.shape[0]):
-     xX=o["num_words"][i]
-     t=o["tokens_representing_words"][i]
-     t=torch.narrow(t,0,0,xX.item())
-     xh=o["tokenid_to_wordid"][i]
-     def lookup(tokid):
-      if tokid==0:
-       return 0
-      else:
-       return xh[tokid]+1
-     xF=m[i]
-     xF=xF[t,:]
-     xF=xF[:,t]
-     xF=xF.transpose(0,1)
-     xF=F.log_softmax(xF,dim=1)
-     xv,tree_score=chu_liu_edmonds(xF.numpy(force=xq).astype(np.double))
-     xv=xv[1:]
-     xl=o['head'][i]
-     xl=xl[t].numpy(force=xq)[1:]
-     xt=np.vectorize(lookup)
-     xl=xt(xl)
-     xc+=(xl==xv).sum()
-     xJ+=xl.shape[0]
-     xD+=1
-    if config.debug:
-     break
-   xb=xk(L)/xH
-   xs=xk(xc)/xJ
-   wandb.log({"dev root accuracy":xb,"dev UAS":xs})
+   oh=0
+   oa=0
+   ol=0
+   oy=0
+   oH=0
+   oC=0
+   try:
+    for oN in tqdm.tqdm(oU,desc="Evaluating"):
+     oP=oN["input_ids"].to(s)
+     ot=oN["attention_mask"].to(s)
+     og=Y(oP,ot) 
+     oR=og.shape[0]
+     od=torch.argmax(og,dim=1).view(-1)
+     om=oN["head"].to(s).view(-1)
+     J,g=q(od,om)
+     oh+=oB(J)
+     oa+=oB(g)
+     ol+=oB(od.shape[0])
+     for i in oA(oR):
+      r=oN["num_words"][i]
+      m=oN["tokens_representing_words"][i]
+      m=torch.narrow(m,0,0,r.item()) 
+      op=oN["tokenid_to_wordid"][i]
+      def L(tokid):
+       if tokid==0:
+        return 0
+       else:
+        return op[tokid]+1 
+      oO=og[i]
+      oO=oO[m,:]
+      oO=oO[:,m]
+      oO=oO.transpose(0,1) 
+      oO=torch.nn.functional.log_softmax(oO,dim=1)
+      oj,oE=chu_liu_edmonds(oO.numpy(force=oY).astype(np.double))
+      oj=oj[1:] 
+      oK=oN['head'][i]
+      oK=oK[m].numpy(force=oY)[1:] 
+      oQ=np.vectorize(L)
+      oK=oQ(oK)
+      oH+=(oK==oj).sum()
+      oC+=oK.shape[0]
+      oy+=1
+      if oy>config.limit_dev:
+       raise oS
+     if config.debug:
+      break 
+   except oS:
+    pass
+   oc=of(oh)/oa
+   oD=of(oH)/oC
+   wandb.log({"dev root accuracy":oc,"dev UAS":oD})
  wandb.finish()
 # Created by pyminifier (https://github.com/liftoff/pyminifier)
 
